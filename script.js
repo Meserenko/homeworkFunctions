aa9a589dbd728aa1e45513a51f729db3ccabf430
Meserenko <meserenko.oleg@gmail.com>
2020-09-22 18:16:11 +0300

    homework

---
+++ script.js
@@ -0,0 +1,97 @@
+// 1. Напишіть функцію, що повертає куб числа.
+// function cube(number) {
+//   return Math.pow(number, 3)
+// }
+//
+// console.log(cube(3));
+//
+
+
+
+// 2. Напишіть функцію, що додає перше число до другого і ділить результат на третє число.
+// function mathOperations(a, b, c) {
+//     return (a + b) / c;
+// }
+//
+// console.log(mathOperations(1,2,3));
+
+
+
+// 3. Напишіть функцію, що приймає число від 1 до 7 і повертає відповідну назву дня (українською).
+//
+// function dayOfWeek(day) {
+//   switch (day) {
+//     case 1:
+//       return("Понеділок");
+//     case 2:
+//       return("Вівторок");
+//     case 3:
+//       return("Середа");
+//     case 4:
+//       return("Четвер");
+//     case 5:
+//       return("П'ятниця");
+//     case 6:
+//       return("Субота");
+//     case 7:
+//       return("Неділя");
+//     default:
+//       console.log('Потрібно ввести в діапазоні від 1 до 7');
+//   }
+//
+// }
+//
+// console.log(dayOfWeek(3));
+
+
+
+// 4. Реалізуйте функцію знаходження факторіала
+// function factorial(x) {
+//   let result = 1;
+//   while(x) {
+//     result *= x--;
+//   }
+//   return result;
+// }
+// console.log( factorial(3) );
+
+
+
+// 5.  Напишіть функцію яка отримує години хвилини та секунди і повертає це число в секундах.
+// let result;
+// const translate = 60;
+// function getTime(hours, minutes, seconds) {
+//   result = (hours * Math.pow(translate, 2)) + (minutes * translate) + seconds;
+//   return result;
+// }
+// console.log(timeGet(3,20,50))
+
+
+// 6.    Написати функцію , яка приймає секунди, і перетворює їх у години хвилини та секунди у форматі «гг:хх:сс». якщо кількість годин більша за 23.59.59 - вивести повідомлення "Більше одного дня".
+// let days, hours, minutes, result;
+// const translate = 60, day = 24;
+// function time(seconds)
+// {
+//   days = Math.floor(seconds / (day* (translate**2)));
+//   if(days >= 1) {
+//     console.log('Більше одного дня');
+//     return days;
+//   } else {
+//     seconds -= days * (day* (translate**2));
+//     hours = Math.floor(seconds / (translate**2));
+//     seconds -= hours * (translate**2);
+//     minutes = Math.floor(seconds / translate);
+//     seconds -= minutes * translate;
+//     return result = `${hours}:${minutes}:${seconds}`;
+//   }
+// }
+// console.log(time(86399));
+
+
+
+// 7. 4 відмінності ерров фанкшина від звичайної функції.
+
+// 1) Синтаксис написання (arg1, arg2, .. argN) => expression;
+// 2) В ерров фанкшин можна не писати return. (arg1, arg2, .. argN) => arg1+arg2;
+// 3) Ерров фанкшин не мають власного this
+// 4) Ерров фанкшин не можуть бути використані для створення об'єктів
